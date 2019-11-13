6eecab5d9f217928f228c971c0a67c91039b3818
Kiril Bonduryansky <55139428+airlivered@users.noreply.github.com>
2019-11-13 21:25:02 +0200

    Update task1.py

--- laboratory1/task1.py
+++ laboratory1/task1.py
@@ -11,8 +11,8 @@ angle = input("Введіть кут радіанної міри: ")
 while not is_num(angle):
     angle = input("Введіть кут радіанної міри: ")
 angle = float(angle)
-yd = angle*57.29577
-deg = int(yd)
-min = int((yd - deg)*60)
-sec = int(((yd - deg)*60 - min)*60)
+float_deg_angle = angle*57.29577
+deg = int(float_deg_angle)
+min = int((float_deg_angle - deg)*60)
+sec = int(((float_deg_angle - deg)*60 - min)*60)
 print("Кут " + str(angle) + " радіан рівний " + str(deg) + " градусам " + str(min) + " хвилинам і " + str(sec) +" секундам")
